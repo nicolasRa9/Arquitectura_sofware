3934faa71bbb5e4dd92742a988b5e9d4b3ac4884
Joaquin0sses <joaquin.osses@mail.udp.cl>
2025-07-03 20:52:23 -0400

    todo de nuevo

--- common/services_mapping.py
+++
@@ -1,7 +0,0 @@
-services_mapping = {
-    "product_manager": "SV001",
-    "location_optimizer": "SV002",
-    "movement_tracker": "SV003",
-    "warehouse_visualizer": "SV004",
-    "alert_report_generator": "SV005"
-}
